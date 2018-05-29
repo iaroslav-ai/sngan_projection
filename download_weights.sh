8067e5ac4e71713cfdac745b8387090d5c262d3f
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2018-05-29 19:45:55 +0200

    Initial version of projection onto GAN manifold

--- download_weights.sh
+++ download_weights.sh
@@ -1,1 +1,1 @@
-gdrive_download 1KY7LocNmD8CqeobESQ4QGUjXpnmXwgPk ResNetGenerator_850000.npz
+wget https://doc-14-8c-docs.googleusercontent.com/docs/securesc/oprau7jlmedilbde3oouq7k1dttuobo8/d8rbm03hj5fln5o3ij02pfmddt8qphuj/1527602400000/06137697440565016501/15021102548602281940/1KY7LocNmD8CqeobESQ4QGUjXpnmXwgPk?e=download -O ResNetGenerator_850000.npz
--- evaluations/gen_interpolated_images.py
+++ evaluations/gen_interpolated_images.py
@@ -15,7 +15,6 @@ sys.path.append(os.path.join(base, '../'))
 from evaluation import gen_images_with_condition
 import yaml
 import source.yaml_utils as yaml_utils
-from source.miscs.random_samples import sample_gaussian
 
 
 def load_models(config):
